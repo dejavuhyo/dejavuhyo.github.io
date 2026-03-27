4318fb33c93d7c038210ac6e38da03b02721cbcd
공대베짱이 <dejavuhyo@gmail.com>
2026-03-27 13:04:02 +0900

    fix: update dist files

--- assets/js/pwa/sw.js
+++ assets/js/pwa/sw.js
@@ -1,87 +1,101 @@
 ---
 layout: compress
-permalink: '/sw.js'
+permalink: /:basename.min.js
 # PWA service worker
 ---
 
-self.importScripts('{{ "/assets/js/data/swcache.js" | relative_url }}');
+const swconfUrl = '{{ '/assets/js/data/swconf.js' | relative_url }}';
 
-const cacheName = 'chirpy-{{ "now" | date: "%Y%m%d.%H%M" }}';
+importScripts(swconfUrl);
+const purge = swconf.purge;
 
-function verifyDomain(url) {
-  for (const domain of allowedDomains) {
-    const regex = RegExp(`^http(s)?:\/\/${domain}\/`);
+function verifyHost(url) {
+  for (const host of swconf.allowHosts) {
+    const regex = RegExp(`^http(s)?://${host}/`);
     if (regex.test(url)) {
       return true;
     }
   }
-
   return false;
 }
 
-function isExcluded(url) {
-  for (const item of denyUrls) {
-    if (url === item) {
-      return true;
+function verifyUrl(url) {
+  if (!verifyHost(url)) {
+    return false;
+  }
+
+  const requestPath = new URL(url).pathname;
+
+  for (const path of swconf.denyPaths) {
+    if (requestPath.startsWith(path)) {
+      return false;
     }
   }
-  return false;
+  return true;
 }
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(resource);
+if (!purge) {
+  swconf.allowHosts.push(location.host);
+}
+
+self.addEventListener('install', (event) => {
+  if (purge) {
+    return;
+  }
+
+  event.waitUntil(
+    caches.open(swconf.cacheName).then((cache) => {
+      return cache.addAll(swconf.resources);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (purge) {
+            return caches.delete(key);
+          } else {
+            if (key !== swconf.cacheName) {
+              return caches.delete(key);
+            }
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((response) => {
+        const url = event.request.url;
+
+        if (purge || event.request.method !== 'GET' || !verifyUrl(url)) {
           return response;
         }
 
-        return fetch(event.request)
-          .then(response => {
-            const url = event.request.url;
+        {% comment %}See: <https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests>{% endcomment %}
+        let responseToCache = response.clone();
 
-            if (event.request.method !== 'GET' ||
-              !verifyDomain(url) ||
-              isExcluded(url)) {
-              return response;
-            }
-
-            /*
-              see: <https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests>
-             */
-            let responseToCache = response.clone();
-
-            caches.open(cacheName)
-              .then(cache => {
-                /* console.log('[sw] Caching new resource: ' + event.request.url); */
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
-      })
-    );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keyList => {
-          return Promise.all(
-            keyList.map(key => {
-              if(key !== cacheName) {
-                return caches.delete(key);
-              }
-            })
-          );
+        caches.open(swconf.cacheName).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
     })
   );
 });
